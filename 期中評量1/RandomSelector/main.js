59ea735f1af3255c3471ca34e55aa7bb85333bf5
a31625802 <83102083+a31625802@users.noreply.github.com>
2021-04-24 10:52:13 +0800

    Delete 期中評量1 directory

--- 期中評量1/RandomSelector/main.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $("input").on("click",function(){
-        //alert("Hi");
-        var numberOfListItem = $("li").length;
-        var randomChildNumber = Math.floor(Math.random()*numberOfListItem);
-        $("h1").text($("li").eq(randomChildNumber).text());
-    });
-});
